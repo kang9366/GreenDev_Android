c52d40a7eb136c5d68630ea682f0df64f7a69ef7
SeungGu Kang <alex9366@naver.com>
2023-07-10 01:25:39 +0900

    MainActivity와 navigation bar 연결

--- app/src/main/java/com/example/greendev/MainActivity.kt
+++ app/src/main/java/com/example/greendev/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.greendev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.fragment.app.Fragment
+import com.example.greendev.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val mainFragment: Fragment = MainFragment()
+    private val campaignFragment: Fragment = CampaignFragment()
+    private val searchFragment: Fragment = SearchFragment()
+    private val profileFragment: Fragment = ProfileFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavigationBar()
+    }
+
+    private fun initNavigationBar() {
+        binding.bottomNavigationView.run {
+            setOnNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.tab_home -> {
+                        changeFragment(mainFragment)
+                    }
+                    R.id.tab_campaign -> {
+                        changeFragment(campaignFragment)
+                    }
+                    R.id.tab_search -> {
+                        changeFragment(searchFragment)
+                    }
+                    R.id.tab_profile -> {
+                        changeFragment(profileFragment)
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.tab_home
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout, fragment)
+            .commit()
     }
 }
