58aff51dba662e3f41ccf37af0dcaccf91e9849d
SeungGu Kang <alex9366@naver.com>
2023-07-16 00:37:51 +0900

    Update SearchFragment.kt : RecyclerView item 검색 기능 구현 및 검색 확인을 위한 테스트 데이터 변경

--- app/src/main/java/com/example/greendev/SearchFragment.kt
+++ app/src/main/java/com/example/greendev/SearchFragment.kt
@@ -1,28 +1,76 @@
 package com.example.greendev
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.greendev.databinding.FragmentSearchBinding
+import java.util.Locale
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapter: RecyclerViewAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentSearchBinding.bind(view)
+        binding = FragmentSearchBinding.bind(view)
 
         val item = ArrayList<CampaignData>()
+        val search_item = ArrayList<CampaignData>()
 
         //recycerview test
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
-        item.add(CampaignData("다다익선 캠페인","스타벅스"))
+        item.add(CampaignData("데보션 캠페인", "SK"))
+        item.add(CampaignData("다다익선 캠페인", "스타벅스"))
+        item.add(CampaignData("다다익선 캠페인", "스타벅스"))
+        item.add(CampaignData("다다익선 캠페인", "스타벅스"))
+        item.add(CampaignData("다다익선 캠페인", "스타벅스"))
+        item.add(CampaignData("다다익선 캠페인", "스타벅스"))
 
-        val adapter = RecyclerViewAdapter(item, R.layout.campaign_item_layout)
+        adapter = RecyclerViewAdapter(item, R.layout.campaign_item_layout)
+
+        binding.searchView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("test", "before")
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("test", "on")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.d("test", "after")
+                val searchText: String = binding.searchView.text.toString()
+                search_item.clear()
+                if(searchText == "") {
+                    adapter.setItem(item)
+                }else {
+                    for(i in 0 until item.size) {
+                        if (item[i].name.toLowerCase().contains(searchText.lowercase(Locale.getDefault()))
+                            || item[i].company.toLowerCase().contains(searchText.lowercase(Locale.getDefault()))) {
+                            search_item.add(item[i])
+                        }
+                        adapter.setItem(search_item)
+                    }
+                }
+            }
+        })
+
+
+        adapter.setOnItemClickListener(object : RecyclerViewAdapter.OnItemClickListener {
+            @SuppressLint("ResourceType")
+            override fun onItemClick(v: View, data: CampaignData, pos: Int) {
+                val transaction = activity?.supportFragmentManager?.beginTransaction()
+                transaction?.apply {
+                    replace(R.id.frameLayout, ApplyFragment())
+                    commit()
+                }
+            }
+        })
+
         binding.recyclerView.adapter = adapter
     }
 
@@ -30,7 +78,6 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 }
