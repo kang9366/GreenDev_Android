5dca6014bf0f092b5ef8afe8876449d8708e5997
SeungGu Kang <alex9366@naver.com>
2023-07-16 00:20:28 +0900

    Update RecyclerViewAdapter.kt : 홈 화면에서 사용되는 캠페인 참여 기록 RecyclerView를 연결하기 위한 Adapter 생성

--- app/src/main/java/com/example/greendev/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/greendev/RecyclerViewAdapter.kt
@@ -58,3 +58,28 @@ class RecyclerViewAdapter(private var items: ArrayList<CampaignData>, private va
         notifyDataSetChanged()
     }
 }
+
+class RecordRecyclerViewAdapter(private val items: ArrayList<RecordData>): Adapter<RecordRecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.record_item_layout, parent, false)
+        return ViewHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.campaign_title.text = item.campaign_title
+        holder.date.text = item.date
+        holder.message.text = item.message
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(v: View): RecyclerView.ViewHolder(v) {
+        private var view: View = v
+        val campaign_title: TextView = view.findViewById(R.id.campaign_title)
+        val message: TextView = view.findViewById(R.id.message)
+        val date: TextView = view.findViewById(R.id.date)
+    }
+}
