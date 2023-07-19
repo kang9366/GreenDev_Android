2cefdc9adcd4937af75850ef80a1b4ce0e6cf015
SeungGu Kang <alex9366@naver.com>
2023-07-19 18:24:21 +0900

    Update RecyclerViewAdapter.kt : 프로필 UI의 뱃지 RecyclerViewAdapter, 터치 이벤트 처리 구현

--- app/src/main/java/com/example/greendev/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/greendev/RecyclerViewAdapter.kt
@@ -1,15 +1,19 @@
 package com.example.greendev
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
 data class CampaignData(val name: String, val company: String)
 data class RecordData(val date: String, val campaign_title: String, val message: String)
+data class BadgeData(val badge: Drawable)
 
 class RecyclerViewAdapter(private var items: ArrayList<CampaignData>, private val layout: Int): Adapter<RecyclerViewAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -81,5 +85,64 @@ class RecordRecyclerViewAdapter(private val items: ArrayList<RecordData>): Adapt
         val campaign_title: TextView = view.findViewById(R.id.campaign_title)
         val message: TextView = view.findViewById(R.id.message)
         val date: TextView = view.findViewById(R.id.date)
+    }
+}
+
+interface ItemTouchHelperListener {
+    fun onItemMove(from: Int, to: Int)
+}
+
+class BadgeRecyclerViewAdapter(private val items: ArrayList<BadgeData>): Adapter<BadgeRecyclerViewAdapter.ViewHolder>(), ItemTouchHelperListener {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BadgeRecyclerViewAdapter.ViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.badge_item, parent, false)
+        return ViewHolder(inflatedView)
+    }
+
+    override fun onBindViewHolder(holder: BadgeRecyclerViewAdapter.ViewHolder, position: Int) {
+        val item = items[position]
+        holder.badge.setImageDrawable(item.badge)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(v: View): RecyclerView.ViewHolder(v){
+        private var view: View = v
+        val badge: ImageView = view.findViewById(R.id.badgeImage)
+    }
+
+    override fun onItemMove(from: Int, to: Int) {
+        val item = items[from]
+        items.removeAt(from)
+        items.add(to, item)
+        notifyItemMoved(from, to)
+    }
+}
+
+class ItemTouchCallback(private val listener: ItemTouchHelperListener) : ItemTouchHelper.Callback() {
+    override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+        val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN or
+                ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        val swipeFlags = 0
+
+        return makeMovementFlags(dragFlags, swipeFlags)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        listener.onItemMove(viewHolder.adapterPosition, target.adapterPosition)
+        return true
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+    }
+
+    override fun isLongPressDragEnabled(): Boolean {
+        return true
     }
 }
