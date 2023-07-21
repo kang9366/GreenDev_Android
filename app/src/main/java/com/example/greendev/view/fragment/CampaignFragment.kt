ee62109e0ae7aef718b8a24d9967a14cd80f87d0
SeungGu Kang <alex9366@naver.com>
2023-07-21 13:22:46 +0900

    Update CampaignFragment.kt : 파일 구조 수정

--- app/src/main/java/com/example/greendev/view/fragment/CampaignFragment.kt
+++ app/src/main/java/com/example/greendev/view/fragment/CampaignFragment.kt
@@ -1,15 +1,11 @@
-package com.example.greendev
+package com.example.greendev.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.fragment.app.Fragment
+import com.example.greendev.R
 
 /**
  * A simple [Fragment] subclass.
@@ -23,10 +19,6 @@ class CampaignFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -51,8 +43,6 @@ class CampaignFragment : Fragment() {
         fun newInstance(param1: String, param2: String) =
             CampaignFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
