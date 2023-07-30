8471c268d340b92ddc24e70ab34d77cd1e37329f
SeungGu Kang <alex9366@naver.com>
2023-07-31 00:09:58 +0900

    Update LoginActivity.kt : 뒤로가기 버튼을 눌렀을때 FinishDialog 생성

--- app/src/main/java/com/example/greendev/view/activity/LoginActivity.kt
+++ app/src/main/java/com/example/greendev/view/activity/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.example.greendev.view.activity
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import com.example.greendev.BindingActivity
 import com.example.greendev.R
+import com.example.greendev.databinding.ActivityLoginBinding
+import com.example.greendev.view.dialog.FinishDialog
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val dialog = FinishDialog(this@LoginActivity)
+            dialog.initDialog()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        this.onBackPressedDispatcher.addCallback(this, callback)
     }
 }
