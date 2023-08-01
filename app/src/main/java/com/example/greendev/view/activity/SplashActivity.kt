05b3a666479a75e0bee3203917dc5fab7fe2aff5
SeungGu Kang <alex9366@naver.com>
2023-08-01 23:19:07 +0900

    Add SplashActivity.kt : 스플래시 화면을 위한 액티비티 생성

---
+++ app/src/main/java/com/example/greendev/view/activity/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.example.greendev.view.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.OnBackPressedCallback
+import com.example.greendev.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private val callback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        this.onBackPressedDispatcher.addCallback(this, callback)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            initLoginActivity()
+        }, DELAY_TIME)
+    }
+
+    private fun initLoginActivity() {
+        Intent(this@SplashActivity, LoginActivity::class.java).apply {
+            startActivity(this)
+        }
+        finish()
+    }
+
+    companion object {
+        private const val DELAY_TIME = 1500L
+    }
+}
