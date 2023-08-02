b3864b6b3af0eb8689624c380573c0d9325b3791
SeungGu Kang <alex9366@naver.com>
2023-08-02 21:23:26 +0900

    Update CertificationFragment.kt : 버튼을 터치해 datePicker 실행

--- app/src/main/java/com/example/greendev/view/fragment/CertificationFragment.kt
+++ app/src/main/java/com/example/greendev/view/fragment/CertificationFragment.kt
@@ -1,14 +1,49 @@
 package com.example.greendev.view.fragment
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import com.example.greendev.BindingFragment
 import com.example.greendev.R
 import com.example.greendev.databinding.FragmentCertificationBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CertificationFragment : BindingFragment<FragmentCertificationBinding>(R.layout.fragment_certification, false) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.backButton?.let { returnToPreviousFragment(it) }
+        binding?.datePickerButton?.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = context?.let {
+            DatePickerDialog(
+                it,
+                { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                    binding?.datePickerButton?.text = setDate(selectedYear, selectedMonth, selectedDay)
+                },
+                year,
+                month,
+                day
+            )
+        }
+        datePickerDialog?.show()
+    }
+
+    private fun setDate(year: Int, month: Int, day: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+        val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return format.format(calendar.time)
     }
 }
