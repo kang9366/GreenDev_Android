76d46fa7441d54fae594180e8236c1f63f499ae6
SeungGu Kang <alex9366@naver.com>
2023-08-04 03:59:09 +0900

    Update CertificationFragment.kt : 갤러리에서 사진 선택 기능 구현

--- app/src/main/java/com/example/greendev/view/fragment/CertificationFragment.kt
+++ app/src/main/java/com/example/greendev/view/fragment/CertificationFragment.kt
@@ -3,24 +3,49 @@ package com.example.greendev.view.fragment
 import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.DatePicker
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.example.greendev.BindingFragment
 import com.example.greendev.R
 import com.example.greendev.databinding.FragmentCertificationBinding
+import com.example.greendev.view.dialog.PhotoDialog
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 class CertificationFragment : BindingFragment<FragmentCertificationBinding>(R.layout.fragment_certification, false) {
+    private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private lateinit var dialog: PhotoDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) {
+            if(it != null) {
+                binding?.imageView?.setImageURI(it)
+                dialog.closeDialog()
+            }else {
+                Log.d("test", "사진 선택 안됨")
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.backButton?.let { returnToPreviousFragment(it) }
         binding?.datePickerButton?.setOnClickListener {
-            showDatePicker()
+            initDatePicker()
+        }
+        binding?.addPhotoButton?.setOnClickListener {
+            dialog = PhotoDialog(context as AppCompatActivity)
+            dialog.initDialog(pickMedia)
         }
     }
 
-    private fun showDatePicker() {
+    private fun initDatePicker() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
