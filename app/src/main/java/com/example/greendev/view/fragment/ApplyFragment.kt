32d81757c4268e4483a0aadf628a89fa44b1212d
SeungGu Kang <alex9366@naver.com>
2023-08-23 01:09:38 +0900

    Update ApplyFragment.kt : Fragment가 생성될때 campaignId를 전달받도록 설정

--- app/src/main/java/com/example/greendev/view/fragment/ApplyFragment.kt
+++ app/src/main/java/com/example/greendev/view/fragment/ApplyFragment.kt
@@ -1,12 +1,20 @@
 package com.example.greendev.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.example.greendev.BindingFragment
 import com.example.greendev.R
 import com.example.greendev.databinding.FragmentApplyBinding
 
-class ApplyFragment : BindingFragment<FragmentApplyBinding>(R.layout.fragment_apply, false) {
+class ApplyFragment(campaignId: Int) : BindingFragment<FragmentApplyBinding>(R.layout.fragment_apply, false) {
+    private var campaignId: Int? = null
+
+    init {
+        this.campaignId = campaignId
+        Log.d("testtt", "id is : $campaignId")
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.backButton?.let { returnToPreviousFragment(it) }
