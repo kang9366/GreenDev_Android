374ac68216eafccf6d6ab363f630cf7b5476a6fd
SeungGu Kang <alex9366@naver.com>
2023-08-23 00:05:24 +0900

    Update RetrofitService.kt : 전체 캠페인 데이터 조회 api 추가

--- app/src/main/java/com/example/greendev/RetrofitService.kt
+++ app/src/main/java/com/example/greendev/RetrofitService.kt
@@ -1,11 +1,10 @@
 package com.example.greendev
 
+import com.example.greendev.model.AllCampaignResponse
 import com.example.greendev.model.ApiResponse
 import com.example.greendev.model.GrassResponse
 import com.example.greendev.model.PostResponse
 import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Header
 
@@ -18,4 +17,7 @@ interface RetrofitService {
 
     @GET("api/v1/members/posts")
     fun getPosts(@Header("Authorization") token: String): Call<PostResponse>
+
+    @GET("api/v1/campaigns?page=0&size=3&sort=joinCount,DESC")
+    fun getAllCampaign(@Header("Authorization") token: String): Call<AllCampaignResponse>
 }
