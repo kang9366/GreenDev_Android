7781dbdfdded2c04fbafd3c6b30aa255477a72fa
SeungGu Kang <alex9366@naver.com>
2023-08-23 02:46:51 +0900

    Update ApplyFragment.kt : 캠페인 상세 정보 조회 api 연결

--- app/src/main/java/com/example/greendev/view/fragment/ApplyFragment.kt
+++ app/src/main/java/com/example/greendev/view/fragment/ApplyFragment.kt
@@ -1,22 +1,57 @@
 package com.example.greendev.view.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.bumptech.glide.Glide
+import com.example.greendev.App
 import com.example.greendev.BindingFragment
 import com.example.greendev.R
+import com.example.greendev.RetrofitBuilder
 import com.example.greendev.databinding.FragmentApplyBinding
+import com.example.greendev.model.DetailCampaignResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApplyFragment(campaignId: Int) : BindingFragment<FragmentApplyBinding>(R.layout.fragment_apply, false) {
     private var campaignId: Int? = null
+    private val retrofitBuilder = RetrofitBuilder.retrofitService
 
     init {
         this.campaignId = campaignId
-        Log.d("testtt", "id is : $campaignId")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding?.backButton?.let { returnToPreviousFragment(it) }
+        initCampaignDetail()
+    }
+
+    private fun initCampaignDetail(){
+        val getDetailCampaign: Call<DetailCampaignResponse> = retrofitBuilder.getDetailCampaign(campaignId!!,"Bearer ${App.preferences.token!!}")
+        getDetailCampaign.enqueue(object : Callback<DetailCampaignResponse>{
+            @SuppressLint("SetTextI18n")
+            override fun onResponse(call: Call<DetailCampaignResponse>, response: Response<DetailCampaignResponse>) {
+                if(response.isSuccessful){
+                    val data = response.body()!!.data
+                    Glide.with(requireContext())
+                        .load(data.campaignImageUrl)
+                        .into(binding?.campaignImage!!)
+
+                    binding?.titleText?.text = data.title
+                    binding?.title?.text = "캠페인 명 : ${data.title}"
+                    binding?.date?.text = "기간 : ${data.date}"
+                    binding?.count?.text = "현재 ${data.joinMemberCount}명이 ${data.joinCount}번 참여중인 캠페인입니다."
+                }
+            }
+
+            override fun onFailure(call: Call<DetailCampaignResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
