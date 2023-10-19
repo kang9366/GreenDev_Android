0ba4e663c924760049d9f0ddd6b721201a6cac7b
SeungGu Kang <alex9366@naver.com>
2023-10-20 01:46:06 +0900

    Update PreferenceUtil.kt : preference 초기화 함수 추가, refreshToken 필드 추가

--- app/src/main/java/com/example/greendev/model/PreferenceUtil.kt
+++ app/src/main/java/com/example/greendev/model/PreferenceUtil.kt
@@ -6,6 +6,12 @@ import android.content.SharedPreferences
 class PreferenceUtil(context: Context) {
     private val pref: SharedPreferences = context.getSharedPreferences("pref", Context.MODE_PRIVATE)
 
+    fun clearAll() {
+        val editor = pref.edit()
+        editor.clear()
+        editor.apply()
+    }
+
     var isLogin: Boolean
         get() = pref.getBoolean("isLogin", false)
         set(value) = pref.edit().putBoolean("isLogin", value).apply()
@@ -13,4 +19,8 @@ class PreferenceUtil(context: Context) {
     var token: String?
         get() = pref.getString("token", null)
         set(value) = pref.edit().putString("token", value).apply()
+
+    var refreshToken: String?
+        get() = pref.getString("refreshToken", null)
+        set(value) = pref.edit().putString("refreshToken", value).apply()
 }
