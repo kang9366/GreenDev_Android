d63a5bebddbb37a51d1025f183ff3748f866f6e5
SeungGu Kang <alex9366@naver.com>
2023-10-20 01:48:43 +0900

    Add AccessTokenResponse.kt

---
+++ app/src/main/java/com/example/greendev/model/AccessTokenResponse.kt
@@ -0,0 +1,11 @@
+package com.example.greendev.model
+
+data class AccessTokenResponse(
+    val status: Int,
+    val message: String,
+    val data: AccessTokenData
+)
+
+data class AccessTokenData(
+    val accessToken: String
+)
