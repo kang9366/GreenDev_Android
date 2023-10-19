7d2c78ed8c8dc595f4fed54e394c49782e391c07
SeungGu Kang <alex9366@naver.com>
2023-10-20 01:47:38 +0900

    Update RetrofitService.kt : API 연결을 위한 함수 추가

--- app/src/main/java/com/example/greendev/RetrofitService.kt
+++ app/src/main/java/com/example/greendev/RetrofitService.kt
@@ -1,18 +1,24 @@
 package com.example.greendev
 
+import com.example.greendev.App.Companion.preferences
+import com.example.greendev.model.AccessTokenResponse
 import com.example.greendev.model.AllCampaignResponse
 import com.example.greendev.model.ApiResponse
 import com.example.greendev.model.BadgeResponse
+import com.example.greendev.model.CampaignPostResponse
+import com.example.greendev.model.CertificationBody
 import com.example.greendev.model.DetailCampaignResponse
 import com.example.greendev.model.GrassResponse
 import com.example.greendev.model.ImageResponse
 import com.example.greendev.model.PostCampaign
 import com.example.greendev.model.PostCampaignResponse
 import com.example.greendev.model.PostResponse
+import com.example.greendev.model.RefreshTokenResponse
 import com.google.gson.JsonObject
 import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Multipart
@@ -28,9 +34,9 @@ interface RetrofitService {
     fun getGrass(@Header("Authorization") token: String): Call<GrassResponse>
 
     @GET("api/v1/members/posts")
-    fun getPosts(@Header("Authorization") token: String): Call<PostResponse>
+    fun getMyPosts(@Header("Authorization") token: String): Call<PostResponse>
 
-    @GET("api/v1/campaigns?page=0&size=3&sort=joinCount,DESC")
+    @GET("api/v1/campaigns?page=0&size=1000&sort=joinCount,DESC")
     fun getAllCampaign(@Header("Authorization") token: String): Call<AllCampaignResponse>
 
     @GET("api/v1/campaigns/{campaignId}")
@@ -48,6 +54,25 @@ interface RetrofitService {
     @GET("api/v1/profile")
     fun getBadgeData(@Header("Authorization") token: String): Call<BadgeResponse>
 
-    @POST("api/v1/token/refresh")
-    fun getRefreshToken(@Header("Cookie") token: String): Call<JsonObject>
+    @POST("api/v1/app/token/refreshToken")
+    fun getRefreshToken(@Body accessToken: String = preferences.token!!): Call<RefreshTokenResponse>
+
+    @POST("api/v1/app/token/refresh")
+    fun reissueToken(@Body refreshToken: String = preferences.refreshToken!!): Call<AccessTokenResponse>
+
+    @POST("api/v1/app/token/blacklist")
+    fun logout(@Body refreshToken: String = preferences.refreshToken!!): Call<JsonObject>
+
+    @GET("api/v1/campaigns/{campaignId}/posts?page=0&size=1000&sort=DESC")
+    fun getCampaignPost(@Path("campaignId") campaignId: Int, @Header("Authorization") token: String): Call<CampaignPostResponse>
+
+    @DELETE("api/v1/posts/{postId}")
+    fun deletePost(@Path("postId") postId: Int, @Header("Authorization") token: String): Call<JsonObject>
+
+    @POST("api/v1/campaigns/{campaignId}/posts")
+    fun postCertification(
+        @Path("campaignId") campaignId: Int,
+        @Header("Authorization") token: String,
+        @Body certifiationData: CertificationBody
+    ): Call<JsonObject>
 }
