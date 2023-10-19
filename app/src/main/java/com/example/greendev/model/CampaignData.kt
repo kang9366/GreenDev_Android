e7f135292af0a341c1804736e22ee32a740d278b
SeungGu Kang <alex9366@naver.com>
2023-10-19 14:54:18 +0900

    Update CampaignData.kt : 코드 리팩토링

--- app/src/main/java/com/example/greendev/model/CampaignData.kt
+++ app/src/main/java/com/example/greendev/model/CampaignData.kt
@@ -1,3 +1,9 @@
 package com.example.greendev.model
 
-data class CampaignData(val name: String, val writer: String, val imageUrl: String, val duration: String, val id: Int)
+data class CampaignData(
+    val name: String,
+    val writer: String,
+    val imageUrl: String?,
+    val duration: String,
+    val id: Int
+)
