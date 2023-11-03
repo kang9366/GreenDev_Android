af47335128c61a7c284c50995cc72b7da4ff217d
SeungGu Kang <alex9366@naver.com>
2023-11-03 14:14:46 +0900

    Fix 플레이스토어 배포를 위해 패키지명 수정, 네이버, 카카오 로그인을 위한 의존성 추가

--- app/src/main/java/com/devocean/greendev/adapter/CampaignAdapter.kt
+++ app/src/main/java/com/devocean/greendev/adapter/CampaignAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.greendev.adapter
+package com.devocean.greendev.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
@@ -8,8 +8,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.greendev.R
-import com.example.greendev.model.CampaignData
+import com.devocean.greendev.R
+import com.devocean.greendev.model.CampaignData
 
 interface OnItemClickListener{
     fun onItemClick(v: View, data: CampaignData, pos : Int)
